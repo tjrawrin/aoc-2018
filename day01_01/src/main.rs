b281ee746bb02472b234b69da15dbaa5188156c0
Tim Warrington <tim.warrin@gmail.com>
2018-12-01 15:49:22 -0700

    update day 01 part 01

--- day01_01/src/main.rs
+++ day01_01/src/main.rs
@@ -2,11 +2,22 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-	let mut count = 0;
+	// Set the current frequency to a starting point of 0
+	let mut current_frequency = 0;
+
+	// Open the input file or display an error if the file failed to open
 	let file = File::open("input.txt").expect("Unable to open the file!");
-	for line in BufReader::new(file).lines() {
-		let num: i32 = line.unwrap().parse().unwrap();
-		count += num;
-		println!("{}", count);
+	// Create a vector of numbers based on each line in the opened input file
+	let frequency_changes: Vec<i32> = BufReader::new(file)
+		.lines()
+		.map(|line| line.unwrap().parse::<i32>().unwrap())
+		.collect();
+
+	// Loop over each frequency change
+	for frequency_change in frequency_changes {
+		// Calculate a new current frequency
+		current_frequency += frequency_change;
 	}
+	// Print the final frequency Result
+	println!("{}", current_frequency);
 }
