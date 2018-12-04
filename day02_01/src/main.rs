f05b0fd565e5838cf14f90b1185e561f734f4e9f
Tim Warrington <tim.warrin@gmail.com>
2018-12-03 23:49:40 -0700

    refactor day 02 part 01

--- day02_01/src/main.rs
+++ day02_01/src/main.rs
@@ -1,32 +1,21 @@
-use std::collections::HashMap;
+use day02_01::calculate_checksum;
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-	let mut total = HashMap::new();
+	// Get command line arguments
+	let args: Vec<String> = env::args().collect();
 
-	let file = File::open("input.txt").expect("Unable to open the file!");
-	for line in BufReader::new(file).lines() {
-		let mut count = HashMap::new();
+	// Get the filename
+	let filename = &args[1];
 
-		let line = line.unwrap();
-		for character in line.bytes() {
-			*count.entry(character).or_insert(0) += 1;
-		}
+	// Open a file containing input values
+	let file = File::open(filename).expect("Unable to open the file!");
 
-		let mut seen_two = false;
-		let mut seen_three = false;
-		for (_key, val) in count.iter() {
-			if val == &2 && !seen_two {
-				seen_two = true;
-				*total.entry("2s").or_insert(0) += 1;
-			}
-			if val == &3 && !seen_three {
-				seen_three = true;
-				*total.entry("3s").or_insert(0) += 1;
-			}
-		}
-	}
+	// Read each line and add it to a vector of Strings
+	let ids: Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
 
-	println!("{}", total["2s"] * total["3s"]);
+	// Print the result
+	println!("{}", calculate_checksum(ids));
 }
